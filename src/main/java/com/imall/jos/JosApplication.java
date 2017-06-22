a02d624fff0a2e90462eabed45356ead7522a43d
MarsOopo <ling_qin_67@hotmail.com>
2017-06-22 23:45:49 +0800

    mapperscan

--- src/main/java/com/imall/jos/JosApplication.java
+++ src/main/java/com/imall/jos/JosApplication.java
@@ -1,11 +1,13 @@
 package com.imall.jos;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.imall.jos.bean.dao")
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class JosApplication {
 
