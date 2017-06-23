3b597b9efdb33cccc305b7758dcddb544cf2dec9
MarsOopo <ling_qin_67@hotmail.com>
2017-06-23 17:31:38 +0800

    springboot+mybatis+velocity

--- src/main/java/com/imall/jos/web/controller/ExampleController.java
+++ src/main/java/com/imall/jos/web/controller/ExampleController.java
@@ -1,9 +1,11 @@
 package com.imall.jos.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.imall.jos.bean.entity.ExampleEntity;
 import com.imall.jos.service.IExampleService;
@@ -37,5 +39,20 @@ public class ExampleController {
 			return 200;
 		}
 		return 500;
+	}
+	
+	@RequestMapping("/hi")
+	public ModelAndView welcome(){
+		ModelAndView mv = new ModelAndView("welcome");
+		mv.getModel().put("toUserName", "李磊");
+		mv.getModel().put("message", "你好啊！");
+		mv.getModel().put("fromUserName", "韩梅梅");
+		mv.getModel().put("time", "2017-06-23");
+		ModelMap mv2 = new ModelMap();
+		mv2.addAttribute("toUserName", "李磊");
+		mv2.addAttribute("message", "你好啊！");
+		mv2.addAttribute("fromUserName", "韩梅梅");
+		mv2.addAttribute("time", "2017-06-23");
+		return mv;
 	}
 }
